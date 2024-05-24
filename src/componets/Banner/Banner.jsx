464175b2adcca48e6a9c2baaa64116350e26b8ab
polasho6 <hossainpolash009@gmail.com>
2024-05-24 22:57:42 +0600

    card Section complet

--- src/componets/Banner/Banner.jsx
+++ src/componets/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="lg:flex gap-36 mx-4  mt-6 bg-gray-300 lg:w-[1700px] lg:h-[750px] rounded-lg">
+    <div className="lg:flex gap-36 mx-auto  mt-6 bg-gray-300 lg:w-[1600px] lg:h-[750px] rounded-lg">
       <div className="mx-16 my-16">
         <h2 className="lg:text-8xl md:text-2xl font-bold lg:mt-60">Books to freshen up<br></br> your bookshelf</h2>
         <button className="btn btn-success lg:mx-60 w-60 h-9 mt-12 text-white text-lg font-medium">View The List</button>
--- src/componets/Books/Books.jsx
+++ src/componets/Books/Books.jsx
@@ -1,11 +1,22 @@
-import { useLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { json, useLoaderData } from "react-router-dom";
+import BookCard from "../BookCard/BookCard";
 
 const Books = () => {
-    // console.log(users)
+    const [book , setbook] = useState([]);
+    useEffect(() =>{
+        fetch('books.json')
+        .then( res => res.json())
+        .then ( data => setbook(data))
+    },[])
     return (
         <div>
-            <h2 className="text-3xl font-bold text-center mt-10">Books:</h2>
-            
+            <h2 className="text-3xl font-bold text-center mt-10">Books:{book.length}</h2>
+            <div className="grid lg:grid-cols-3 mt-10 gap-10">
+                {
+                    book.map( book => <BookCard key={book.id} book={book}></BookCard>)
+                }
+            </div>
         </div>
         
     );
